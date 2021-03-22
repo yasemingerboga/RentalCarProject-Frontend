dd875b1d56e8cb0afb2fbcf7bdb100523c3ebcbe
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-03-22 14:27:52 +0300

    update

--- rentalCar/src/app/services/color.service.ts
+++ rentalCar/src/app/services/color.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ColorResponseModel } from '../models/colorResponseModel';
+import { Color } from '../models/color';
+import { ListResponseModel } from '../models/listResponseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class ColorService {
 
   constructor(private httpClient: HttpClient) { }
   apiUrl = 'https://localhost:44336/api/colors/getall';
-  getColors():Observable<ColorResponseModel> {
-    return this.httpClient.get<ColorResponseModel>(this.apiUrl);
+  getColors():Observable<ListResponseModel<Color>> {
+    return this.httpClient.get<ListResponseModel<Color>>(this.apiUrl);
   }
 }
